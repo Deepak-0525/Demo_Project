855661065885add4ae73ac4045942b1652cb0699
Deepak-0525 <deepaksaravanan45@gmail.com>
2024-07-09 22:50:40 +0530

    commited

--- JavaScript/Exercise2.js
+++ JavaScript/Exercise2.js
@@ -2,4 +2,11 @@ function add(a,b)
 {
     console.log(a+b, a-b)
 }
+function sum(c,d,e)
+{
+    console.log("Multiples of ="+(c*d*e),"Additon of ="+ (c+d+e),"Division of ="+ (c/d/e))
+   
+
+}
+sum(10,20,35)
 add(20,30)
